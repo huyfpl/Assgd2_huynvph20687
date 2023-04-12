073be716c79b51e053aef94ae1ecd042d74f6cd6
Huyfpl <94282645+huyfpl@users.noreply.github.com>
2023-04-12 18:21:04 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   'secret': 'huy tdb ff ',
-  'database': 'nhap link moi'
+  'database': 'mongodb+srv://huynvph20687:c1sas2Dnu6fPDo6F@mongo-huynvph20687.f01mcft.mongodb.net/test_mongodb?retryWrites=true&w=majority'
   // ví dụ : mongodb+srv://huynvph20687:c1sas2Dnu6fPDo6F@mongo-huynvph20687.f01mcft.mongodb.net/test_mongodb?retryWrites=true&w=majority
 };
